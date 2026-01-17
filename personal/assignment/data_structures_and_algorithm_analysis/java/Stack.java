d8c4f7b2859817467f0902387cb1c27bff877583
Babang <mubarak.m@gojek.com>
2026-01-17 13:14:42 +0700

    forum

---
+++ personal/assignment/data_structures_and_algorithm_analysis/java/Stack.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class Stack {
+    private int maxSize;
+    private Object[] stackArray;
+    private int top;
+    public Stack(int size) {
+        maxSize = size;
+        stackArray = new Object[maxSize];
+        top = -1;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(2);
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack.toString());
+        stack.push(30);
+    }
+
+    public void push(Object value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push element.");
+            return;
+        }
+        top++;
+        stackArray[top] = value;
+    }
+
+    public Object pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop element.");
+            return -1;
+        }
+        int oldTop = top;
+        top--;
+        stackArray[oldTop] = null;
+        return stackArray[oldTop];
+    }
+
+    public Object peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek element.");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Stack: " + Arrays.toString(stackArray);
+    }
+}
